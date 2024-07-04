d3fd39d9932c1dc97ed4cff7333dd48228576a77
Maksim <maksimpv1@bk.ru>
2024-07-04 17:56:58 +0300

    start

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Button, Alert, TouchableHighlight } from 'react-native';
 
 export default function App() {
+  const handleClick = () => {
+    console.log('Hi click')
+  }
+  const handleAlert = () => {
+    Alert.alert("Title", "Main Message", [
+      {text: "Да", onPress: ()=> console.log("Yes")},
+      {text: "Отмена", onPress: ()=> console.log("No")}
+    ])
+  }
+  const handleAlertTwo = () => {
+    Alert.prompt("Title", "Main Message", text => console.log(text))
+  }
   return (
-    <View style={styles.container}>
-      <Text>Hi</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.container}>
+        <SafeAreaView>
+          <Text onPress={handleAlertTwo} >Hi</Text>
+          <Button title={'Hierererw'}
+            onPress={handleAlert}
+            style={styles.button} />
+            <TouchableHighlight onPress={handleClick}>
+              <Image/>
+            </TouchableHighlight>
+        </SafeAreaView>
+        <StatusBar style="auto" />
+      </View>
   );
 }
 
